d1caeae22281d8599872e7db8634b8a93b2deb01
SriNithya2004 <soupatisrinithya2004@gmail.com>
2024-01-23 20:28:55 +0530

    mis_designs

---
+++ app/src/main/java/com/example/mis/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.mis
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, DashboardFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_dash)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_dash -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, DashboardFragment()).commit()
+            R.id.nav_exptl -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, exploreFragment()).commit()
+            R.id.nav_proj -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, projectsFragment()).commit()
+            R.id.nav_rec -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, recordsFragment()).commit()
+            R.id.nav_set -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+            R.id.nav_logout -> Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
